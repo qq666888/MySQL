8e34996af9d9d7df47f14b4c56042a20bca24093
qq666888 <1096161287@qq.com>
2017-05-15 17:37:09 +0800

    show

--- sql/day.sql
+++ sql/day.sql
@@ -1,1 +1,18 @@
 SHOW DATABASES ;
+CREATE DATABASE db_1702;
+
+DROP DATABASE db_1702;
+
+USE db_1702;
+
+SHOW TABLES;
+CREATE TABLE t_student(
+  son VARCHAR(10),
+  sname VARCHAR(10),
+  gender char(1),
+  age int(2)
+);
+SELECT *
+FROM t_student;
+INSERT INTO t_student
+    VALUES ('2017001','张三','男','18')
